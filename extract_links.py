c42822ef5aa579babc836b4f9b4b7e3256cf06bd
ecaromeo <89895671+ecaromeo@users.noreply.github.com>
2023-06-01 23:32:17 +0200

    link analysis

--- extract_links.py
+++ extract_links.py
@@ -19,13 +19,15 @@ def extract_links(description):
 with open("videos_description.json") as ff:
     videos = json.load(ff)
 
+with open("link_domains.json") as ff:
+    domains = json.load(ff)
 
 videos_len = len(videos)
-domains = []
+videos = videos[len(domains):]
 
 for _, descr in videos:
     print(f'executing video number {len(domains)+1} out of {videos_len}')
     domains.append(extract_links(descr))
 
-with open('link_domains.json','w') as ff:
+with open('link_domains2.json','w') as ff:
     json.dump(domains, ff)
